06f7ff7ae36d270109511614f1e8fd8227da7b6f
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-02-12 00:21:17 +0700

    add splitAuthorizationBearer to AuthService

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+    public String splitAuthorizationBearer(String header) {
+        return header.split(" ")[1];
+    }
 
     public boolean validateJwtToken(String token) {
         if (token.equals("VALID-JWT-TOKEN"))
