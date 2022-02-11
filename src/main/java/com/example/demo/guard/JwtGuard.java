52e7b11b7741fe8be082086292b678d29b33a38e
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-02-12 00:22:28 +0700

    implement process method in JwtGuard

--- src/main/java/com/example/demo/guard/JwtGuard.java
+++ src/main/java/com/example/demo/guard/JwtGuard.java
@@ -1,9 +1,24 @@
 package com.example.demo.guard;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.example.demo.service.AuthService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class JwtGuard implements IGuardHandler {
+    @Autowired
+    AuthService authService;
 
     @Override
-    public boolean process() {
+    public boolean process(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        String accessToken = authService.splitAuthorizationBearer(authorizationHeader);
+
+        if (authService.validateJwtToken(accessToken) == true) {
+            return true;
+        }
+
         return false;
     }
 
