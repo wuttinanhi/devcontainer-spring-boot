0ab2475a201f3c88728726feae6594086d293545
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-02-12 00:19:58 +0700

    IGuardHandler process now receive request

--- src/main/java/com/example/demo/guard/IGuardHandler.java
+++ src/main/java/com/example/demo/guard/IGuardHandler.java
@@ -1,5 +1,7 @@
 package com.example.demo.guard;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface IGuardHandler {
-    public boolean process();
+    public boolean process(HttpServletRequest request);
 }
